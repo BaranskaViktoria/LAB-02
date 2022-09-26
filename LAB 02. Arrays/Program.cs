a6f7b0276abe26499deabbc34cd6511acfd6e98a
Viktoria <vika74860@gmail.com>
2022-09-26 09:55:01 +0300

    lllll

--- LAB 02. Arrays/Program.cs
+++ LAB 02. Arrays/Program.cs
@@ -6,7 +6,7 @@ namespace LAB_02._Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!;lm;l,l;");
         }
     }
 }
